daa98983c8bbe744a7527ddc38d9039197fa9873
Elias Lima <eliasmoraislima@gmail.com>
2021-12-02 16:45:28 -0100

    create seeder for create new admin and normal user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,67 @@
 <?php
 
-namespace Database\Seeders;
+  
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\User;
+
+   
+
+class CreateUsersSeeder extends Seeder
+
 {
+
     /**
-     * Seed the application's database.
+
+     * Run the database seeds.
+
      *
+
      * @return void
+
      */
+
     public function run()
+
     {
-        // \App\Models\User::factory(10)->create();
+
+        $user = [
+
+            [
+
+               'name'=>'Admin',
+
+               'email'=>'admin@itsolutionstuff.com',
+
+                'is_admin'=>'1',
+
+               'password'=> bcrypt('123456'),
+
+            ],
+
+            [
+
+               'name'=>'User',
+
+               'email'=>'user@itsolutionstuff.com',
+
+                'is_admin'=>'0',
+
+               'password'=> bcrypt('123456'),
+
+            ],
+
+        ];
+
+  
+
+        foreach ($user as $key => $value) {
+
+            User::create($value);
+
+        }
+
     }
+
 }

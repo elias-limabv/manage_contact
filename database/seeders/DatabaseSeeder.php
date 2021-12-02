377e48f66779e35b13fd4a774cac52b4deca910f
Elias Lima <eliasmoraislima@gmail.com>
2021-12-02 17:12:17 -0100

    update file DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,67 +1,18 @@
 <?php
 
-  
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-use App\Models\User;
-
-   
-
-class CreateUsersSeeder extends Seeder
-
+class DatabaseSeeder extends Seeder
 {
-
     /**
-
-     * Run the database seeds.
-
+     * Seed the application's database.
      *
-
      * @return void
-
      */
-
     public function run()
-
     {
-
-        $user = [
-
-            [
-
-               'name'=>'Admin',
-
-               'email'=>'admin@itsolutionstuff.com',
-
-                'is_admin'=>'1',
-
-               'password'=> bcrypt('123456'),
-
-            ],
-
-            [
-
-               'name'=>'User',
-
-               'email'=>'user@itsolutionstuff.com',
-
-                'is_admin'=>'0',
-
-               'password'=> bcrypt('123456'),
-
-            ],
-
-        ];
-
-  
-
-        foreach ($user as $key => $value) {
-
-            User::create($value);
-
-        }
-
+        // \App\Models\User::factory(10)->create();
     }
-
 }
